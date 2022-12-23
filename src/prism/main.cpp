b228c0810e163547e02e9f52c5a4db6e75f1de18
EigenEmara <eigen.emara@gmail.com>
2022-12-23 09:51:52 +0200

    add unvpp as a submodule

--- src/prism/main.cpp
+++ src/prism/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <fmt/core.h>
+#include <unvpp/unvpp.h>
 
 int main() {
+    auto v = unv::Vertex {
+        0.0,
+        0.0,
+        0.0,
+    };
+
+    fmt::print("{}", v.x);
     fmt::print("Hello, {}\n", "World!");
 }
