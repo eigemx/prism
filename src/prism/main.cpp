6ed6df6bb62a7444289661e3f769d46453e5bc38
eigenemara <eigen.emara@gmail.com>
2022-12-23 11:59:54 +0200

    adding clang format and tidy files

--- src/prism/main.cpp
+++ src/prism/main.cpp
@@ -1,14 +1,20 @@
-#include <iostream>
-#include <Eigen/Dense>
 #include <fmt/core.h>
 #include <unvpp/unvpp.h>
 
+
+#include <Eigen/Dense>
+#include <iostream>
+
 int main() {
-    auto v = unv::Vertex {
+    auto v = unv::Vertex{
         0.0,
         0.0,
         0.0,
     };
+
+    auto m = v;
+    std::string x;
+    x.clear();
 
     fmt::print("{}", v.x);
     fmt::print("Hello, {}\n", "World!");
