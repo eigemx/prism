8846e93a4f1dd519d88ed20968f8566bbf7738c2
eigemx <eigen.emara@gmail.com>
2022-12-31 10:41:09 +0200

    from_unv tetra & wedge cells implementation

--- src/prism/mesh/face.h
+++ src/prism/mesh/face.h
@@ -18,17 +18,19 @@ public:
     auto inline area() const -> const double& { return _area; }
     auto inline normal() const -> const Vector3d& { return _normal; }
     auto inline center() const -> const Vector3d& { return _center; }
-    auto inline id() const -> const std::size_t& { return _id; }
-    auto inline owner() const -> const std::size_t& { return _owner; }
+
+    auto inline id() const -> std::size_t { return _id; }
+    void inline set_id(std::size_t face_id) { _id = face_id; }
+
+    auto inline owner() const -> std::size_t { return _owner; }
     void inline set_owner(std::size_t owner_id) { _owner = owner_id; }
-    auto inline neighbor() const -> const std::optional<std::size_t>& {
-        return _neighbor;
-    }
+
+    auto inline neighbor() const -> const std::optional<std::size_t>& { return _neighbor; }
     auto inline set_neighbor(std::size_t nei_id) { _neighbor = nei_id; }
 
 private:
-    void set_face_attributes(const std::vector<Vector3d>& face_vertices,
-                             Vector3d& geo_center, std::size_t vertices_count);
+    void set_face_attributes(const std::vector<Vector3d>& face_vertices, Vector3d& geo_center,
+                             std::size_t vertices_count);
     std::size_t vertices_count;
     double _area {0.0};
     Vector3d _normal {0.0, 0.0, 0.0};
