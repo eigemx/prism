b77192c76bb90c1304a3b0b568f0fef5225312da
eigemx <eigen.emara@gmail.com>
2023-01-11 11:16:09 +0200

    add toml++ as a dependency

--- src/prism/main.cpp
+++ src/prism/main.cpp
@@ -1,5 +1,6 @@
 #include <fmt/color.h>
 #include <fmt/core.h>
+#include <toml++/toml.h>
 #include <unvpp/unvpp.h>
 
 #include <Eigen/Dense>
@@ -7,11 +8,13 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <string_view>
 #include <tuple>
 #include <utility>
 
 #include "mesh/from_unv.h"
 
+using namespace std::string_view_literals;
 
 auto main(int argc, char* argv[]) -> int {
     std::string filename;
@@ -26,4 +29,23 @@ auto main(int argc, char* argv[]) -> int {
     // test UNV library functionality
     auto pmesh = prism::mesh::UnvToPMesh(filename);
     pmesh.report_mesh_stats();
+
+    static constexpr std::string_view some_toml = R"(
+        [library]
+        name = "toml++"
+        authors = ["Mark Gillard <mark.gillard@outlook.com.au>"]
+        cpp = 17
+    )"sv;
+
+    try {
+        // parse directly from a string view:
+        {
+            toml::table tbl = toml::parse(some_toml);
+            std::cout << tbl << "\n";
+        }
+
+    } catch (const toml::parse_error& err) {
+        std::cerr << "Parsing failed:\n" << err << "\n";
+        return 1;
+    }
 }
