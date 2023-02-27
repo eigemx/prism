585cf0fabac6ede4c85dbc29326c69c149edf345
eigenemara <eigen.emara@gmail.com>
2023-02-27 17:12:35 +0200

    implement apply_interior for diffusion::Linear

--- src/prism/diffusion/diffusion.cpp
+++ src/prism/diffusion/diffusion.cpp
@@ -1,17 +1,46 @@
 #include "diffusion.h"
 
+#include <cmath>
+
+#include "../print.h"
+
 namespace prism::diffusion {
 
 void Linear::apply_interior(const mesh::Cell& cell, const mesh::Face& face) const {
     auto cell_id = cell.id();
-    auto adj_cell_id = face.neighbor();
 
     // does `cell` own `face` or it is a neighbor?
     auto is_owned = (face.owner() == cell_id);
+    auto own_factor = is_owned ? 0.0 : 1.0;
+
+    // get adjacent cell shaing `face` with `cell`
+    auto adj_cell_id = is_owned ? face.neighbor().value() : face.owner();
+    auto adj_cell = _mesh.cells()[adj_cell_id];
+
+    // face area vector
+    auto S_f = face.area() * face.normal();
+
+    // vector joining the centers of the two cells
+    auto d_CF = adj_cell.center() - cell.center();
+    auto d_CF_norm = d_CF.norm();
+
+    // unit vector in d_CF direction
+    auto e = d_CF / d_CF_norm;
+
+    // orthogonal-like normal vector E_f using over-relaxed approach
+    auto E_f = ((S_f.dot(S_f) / (e.dot(S_f) + 1e-8))) * e;
+
+    // diffusion factor
+    auto g_diff = (-E_f.norm() * _kappa) / (d_CF_norm + 1e-8);
+    g_diff *= std::pow(-1., own_factor);
+
+    // update coefficients matrix
+    _coeffs.coeffRef(cell_id, cell_id) += -g_diff;
+    _coeffs.coeffRef(cell_id, adj_cell_id) += g_diff;
 }
 
 void Linear::apply_boundary(const mesh::Cell& cell, const mesh::Face& face) const {
-    auto cell_id = cell.id();
+    /*auto cell_id = cell.id();
     const auto& boundary_patch = _mesh.face_boundary_patch(face);
 
     switch (boundary_patch.type()) {
@@ -23,6 +52,6 @@ void Linear::apply_boundary(const mesh::Cell& cell, const mesh::Face& face) cons
 
         default:
             throw std::runtime_error("Non-implemented boundary patch type");
-    }
+    }*/
 }
-} // namespace prism
+} // namespace prism::diffusion
--- src/prism/equation.cpp
+++ src/prism/equation.cpp
@@ -5,9 +5,11 @@ ConservedScalarSteadyEquation::ConservedScalarSteadyEquation(std::string scalar_
                                                              mesh::PMesh& mesh)
     : _scalar_name(std::move(scalar_name)), _mesh(mesh) {
     // reserve space for the coefficient matrix with the size of mesh cells
-    _coeff_matrix.reserve(mesh.cells().size());
+    auto n_cells = mesh.cells().size();
+    _coeff_matrix = SparseMatrix(n_cells, n_cells);
 
     // reserve space for the right hand side vector with the size of mesh cells
     _b.resize(mesh.cells().size());
+    _b.setZero();
 }
 } // namespace prism
