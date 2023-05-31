24032368a38cfb7835f60e75b665c7f318aadd6e
eigemx <eigen.emara@gmail.com>
2023-05-31 17:45:14 +0200

    reimplement ScalarField & VectorField

--- src/prism/field.cpp
+++ src/prism/field.cpp
@@ -6,86 +6,75 @@ namespace prism {
 
 // ScalarField constructors
 ScalarField::ScalarField(std::string name, const mesh::PMesh& mesh)
-    : _mesh(mesh), _name(std::move(name)), _data(VectorXd::Zero(mesh.cells().size())) {}
+    : _mesh(mesh),
+      _name(std::move(name)),
+      _data(std::make_shared<VectorXd>(mesh.cells().size())) {}
+
 
 ScalarField::ScalarField(std::string name, const mesh::PMesh& mesh, VectorXd data)
-    : _mesh(mesh), _name(std::move(name)), _data(std::move(data)) {}
+    : _mesh(mesh), _name(std::move(name)), _data(std::make_shared<VectorXd>(std::move(data))) {}
+
 
 ScalarField::ScalarField(std::string name, const mesh::PMesh& mesh, double value)
     : _mesh(mesh),
       _name(std::move(name)),
-      _data(VectorXd::Constant(mesh.cells().size(), value)) {}
+      _data(std::make_shared<VectorXd>(VectorXd::Ones(mesh.cells().size()) * value)) {}
 
-void ScalarField::update_parent_vec_field() {
-    if (_parent_vec_field == nullptr) {
-        warn(format("ScalarField::update_parent_vec_field: '{}' has no parent vector field.",
-                    _name));
-        return;
-    }
 
-    if (_name == _parent_vec_field->name() + "_x") {
-        _parent_vec_field->data().col(0) = _data;
-    }
-
-    else if (_name == _parent_vec_field->name() + "_y") {
-        _parent_vec_field->data().col(1) = _data;
-    }
-
-    else if (_name == _parent_vec_field->name() + "_z") {
-        _parent_vec_field->data().col(2) = _data;
-    }
-
-    else {
-        throw std::runtime_error(
-            format("ScalarField::update_parent_vec_field: {} is not a component of {}",
-                   _name,
-                   _parent_vec_field->name()));
-    }
-}
+ScalarField::ScalarField(std::string name,
+                         const mesh::PMesh& mesh,
+                         std::shared_ptr<VectorXd> data)
+    : _mesh(mesh), _name(std::move(name)), _data(std::move(data)) {}
 
 
 // VectorField constructors and methods
 VectorField::VectorField(std::string name, const mesh::PMesh& mesh)
-    : _mesh(mesh), _name(std::move(name)), _data(MatrixX3d::Zero(mesh.cells().size(), 3)) {}
+    : _mesh(mesh),
+      _name(std::move(name)),
+      _x(std::make_shared<VectorXd>(mesh.cells().size())),
+      _y(std::make_shared<VectorXd>(mesh.cells().size())),
+      _z(std::make_shared<VectorXd>(mesh.cells().size())) {}
 
-VectorField::VectorField(std::string name, const mesh::PMesh& mesh, Vector3d data)
-    : _mesh(mesh), _name(std::move(name)) {
-    _data.resize(mesh.cells().size(), 3);
-    _data.col(0) = VectorXd::Constant(mesh.cells().size(), data(0));
-    _data.col(1) = VectorXd::Constant(mesh.cells().size(), data(1));
-    _data.col(2) = VectorXd::Constant(mesh.cells().size(), data(2));
-}
 
-VectorField::VectorField(std::string name, const mesh::PMesh& mesh, MatrixX3d data)
-    : _mesh(mesh), _name(std::move(name)), _data(std::move(data)) {}
+VectorField::VectorField(std::string name, const mesh::PMesh& mesh, const Vector3d& data)
+    : _mesh(mesh),
+      _name(std::move(name)),
+      _x(std::make_shared<VectorXd>(VectorXd::Ones(mesh.cells().size()) * data(0))),
+      _y(std::make_shared<VectorXd>(VectorXd::Ones(mesh.cells().size()) * data(1))),
+      _z(std::make_shared<VectorXd>(VectorXd::Ones(mesh.cells().size()) * data(2))) {}
+
+VectorField::VectorField(std::string name, const mesh::PMesh& mesh, const MatrixX3d& data)
+    : _mesh(mesh),
+      _name(std::move(name)),
+      _x(std::make_shared<VectorXd>(data.col(0))),
+      _y(std::make_shared<VectorXd>(data.col(1))),
+      _z(std::make_shared<VectorXd>(data.col(2))) {}
 
 VectorField::VectorField(std::string name, const mesh::PMesh& mesh, double value)
-    : _mesh(mesh), _name(std::move(name)) {
-    _data = MatrixX3d::Constant(mesh.cells().size(), 3, value);
+    : _mesh(mesh),
+      _name(std::move(name)),
+      _x(std::make_shared<VectorXd>(VectorXd::Ones(mesh.cells().size()) * value)),
+      _y(std::make_shared<VectorXd>(VectorXd::Ones(mesh.cells().size()) * value)),
+      _z(std::make_shared<VectorXd>(VectorXd::Ones(mesh.cells().size()) * value)) {}
+
+auto VectorField::data() const -> MatrixX3d {
+    MatrixX3d data(_mesh.cells().size(), 3);
+    data.col(0) = *_x;
+    data.col(1) = *_y;
+    data.col(2) = *_z;
+    return data;
 }
 
 auto VectorField::x() -> ScalarField {
-    auto x = ScalarField {_name + "_x", _mesh};
-    x.set_parent_vec_field(this);
-    x.data() = _data.col(0);
-
-    return x;
+    return {_name + "_x", _mesh, _x};
 }
 
 auto VectorField::y() -> ScalarField {
-    auto y = ScalarField {_name + "_y", _mesh};
-    y.set_parent_vec_field(this);
-    y.data() = _data.col(1);
-
-    return y;
+    return {_name + "_y", _mesh, _y};
 }
 
 auto VectorField::z() -> ScalarField {
-    auto z = ScalarField {_name + "_z", _mesh};
-    z.set_parent_vec_field(this);
-    z.data() = _data.col(2);
-
-    return z;
+    return {_name + "_z", _mesh, _z};
 }
 
 } // namespace prism
--- src/solvers/diffsolver/main.cpp
+++ src/solvers/diffsolver/main.cpp
@@ -1,5 +1,6 @@
 #include <prism/core.h>
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -39,6 +40,14 @@ auto main(int argc, char* argv[]) -> int {
     solver.solve(eqn, 1000, 1e-10);
 
     prism::export_field(eqn.scalar_field(), "solution.vtu");
+
+    // define a vector field over our mesh
+    //auto V = VectorField("velocity", mesh, Vector3d(1.0, 0.0, 0.0));
+    //V[0] = Vector3d(3.0, 2.0, 3.0);
+    //
+    //print("V_x[0] = {}\n", V.x()[0]);
+    ////std::cout << V[0] << std::endl;
+    //fmt::print("V[0] = {}\n", V[0]);
 
     return 0;
 }
