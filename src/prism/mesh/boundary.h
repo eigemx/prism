9034ef1cb2724efa517ff225ee95254f7346fc26
eigenemara <eigen.emara@gmail.com>
2023-06-03 13:00:57 +0200

    revamp bounday condition functionality

--- src/prism/mesh/boundary.h
+++ src/prism/mesh/boundary.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <filesystem>
-#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -20,48 +19,54 @@ enum class BoundaryPatchType {
     Unknown, // for error handling
 };
 
-enum class BoundaryConditionType { Scalar, Vector };
+enum class BoundaryConditionValueType {
+    Nil, // Empty or symmetry
+    Scalar,
+    Vector
+};
 
 using BoundaryConditionData = std::variant<double, Vector3d>;
 
 class BoundaryCondition {
   public:
-    BoundaryCondition(std::string name, BoundaryConditionType type, BoundaryConditionData data)
-        : _name(std::move(name)), _type(type), _data(std::move(data)) {}
+    BoundaryCondition(BoundaryConditionValueType type,
+                      BoundaryConditionData data,
+                      BoundaryPatchType patch_type)
+        : _type(type), _data(std::move(data)), _patch_type(patch_type) {}
 
-    auto name() const noexcept -> const std::string& { return _name; }
-    auto type() const noexcept -> BoundaryConditionType { return _type; }
+    auto type() const noexcept -> BoundaryConditionValueType { return _type; }
     auto data() const noexcept -> const BoundaryConditionData& { return _data; }
+    auto patch_type() const noexcept -> BoundaryPatchType { return _patch_type; }
 
   private:
     std::string _name;
-    BoundaryConditionType _type;
+    BoundaryConditionValueType _type;
     BoundaryConditionData _data;
+    BoundaryPatchType _patch_type;
 };
-
-using BoundaryConditions = std::vector<BoundaryCondition>;
 
 class BoundaryPatch {
   public:
     BoundaryPatch() = delete;
 
-    BoundaryPatch(std::string name, BoundaryConditions attributes, BoundaryPatchType type)
-        : _name(std::move(name)), _bcs(std::move(attributes)), _type(type) {};
+    BoundaryPatch(std::string name, std::map<std::string, BoundaryCondition> field_name_to_bc_map)
+        : _name(std::move(name)), _field_name_to_bc_map(std::move(field_name_to_bc_map)) {}
 
     auto name() const noexcept -> const std::string& { return _name; }
-    auto type() const noexcept -> BoundaryPatchType { return _type; }
-    auto boundary_conditions() const noexcept -> const BoundaryConditions& { return _bcs; }
-    auto get_scalar_bc(const std::string& name) const -> double;
-    auto get_vector_bc(const std::string& name) const -> Vector3d;
+
+    // this method returns the boundary condition for a scalar field given its name
+    auto get_bc(const std::string& field_name) const -> const BoundaryCondition&;
+
+    auto get_scalar_bc(const std::string& field_name) const -> double;
+    auto get_vector_bc(const std::string& field_name) const -> Vector3d;
 
   private:
     std::string _name;
-    BoundaryPatchType _type;
-    BoundaryConditions _bcs;
+    std::map<std::string, BoundaryCondition> _field_name_to_bc_map;
 };
 
-auto read_boundary_conditions(const std::filesystem::path& path,
-                              const std::vector<std::string_view>& boundary_names)
+auto read_boundary_data_file(const std::filesystem::path& path,
+                             const std::vector<std::string_view>& boundary_names)
     -> std::vector<BoundaryPatch>;
 
 } // namespace prism::mesh
