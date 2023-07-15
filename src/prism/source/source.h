3d04469b03fc9f7acc0c7f93d7699a1e65ae1ffe
eigemx <eigen.emara@gmail.com>
2023-07-15 02:25:28 +0200

    implement ImplicitPhi source scheme

--- src/prism/source/source.h
+++ src/prism/source/source.h
@@ -6,40 +6,39 @@
 #include "../fvscheme.h"
 
 namespace prism::source {
-// General note: source term is assumed to be always on the right hand side of the conserved equation.
+// source term is assumed to be always on the right hand side of the conserved equation.
+
+enum class SourceSign { Positive, Negative };
 
 class ConstantScalar : public FVScheme {
   public:
-    ConstantScalar() = delete;
     ConstantScalar(ScalarField& phi) : _phi(phi), FVScheme(phi.mesh().n_cells()) {}
     ConstantScalar(ScalarField&& phi) = delete;
-
-    ConstantScalar(const ConstantScalar& other) = delete;
-    ConstantScalar(ConstantScalar&& other) = delete;
-    auto operator=(const ConstantScalar& other) -> ConstantScalar& = delete;
-    auto operator=(ConstantScalar&& other) -> ConstantScalar& = delete;
-    ~ConstantScalar() = default;
 
     auto requires_correction() const -> bool override { return false; }
 
   private:
-    void apply_interior(const mesh::Cell& cell, const mesh::Face& face) override;
+    // NOLINTNEXTLINE (`face` is unused, but required by interface)
+    void inline apply_interior(const mesh::Cell& cell, const mesh::Face& face) {
+        auto q = _phi[cell.id()];
+        rhs_vector()(cell.id()) = q * cell.volume();
+    }
     void inline apply_boundary(const mesh::Cell& cell, const mesh::Face& face) override {}
 
     ScalarField& _phi;
 };
 
-class Phi : public FVScheme {
+template <SourceSign Sign = SourceSign::Positive>
+class ImplicitPhi : public FVScheme {
   public:
-    Phi(ScalarField& phi) : _phi(phi), FVScheme(phi.mesh().n_cells()), _phi_old(phi) {}
-    Phi(ScalarField&& phi) : _phi(phi), FVScheme(phi.mesh().n_cells()), _phi_old(phi) {}
+    ImplicitPhi(ScalarField& phi) : _phi(phi), FVScheme(phi.mesh().n_cells()) {}
+    ImplicitPhi(ScalarField&& phi) = delete;
 
   private:
     void apply_interior(const mesh::Cell& cell, const mesh::Face& face) override;
     void inline apply_boundary(const mesh::Cell& cell, const mesh::Face& face) override {}
 
     ScalarField& _phi;
-    ScalarField _phi_old;
 };
 
 } // namespace prism::source
