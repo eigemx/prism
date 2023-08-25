c37b185a9f95dd1baf8748fff81727d37019ecb2
eigemx <eigen.emara@gmail.com>
2023-08-25 17:14:07 +0200

    implement ImplicitPhi source scheme

--- src/prism/source/source.cpp
+++ src/prism/source/source.cpp
@@ -1,35 +1,27 @@
 #include "source.h"
 
 namespace prism::source {
-ConstantScalar::ConstantScalar(ScalarField& phi) : _phi(phi), FVScheme(phi.mesh().n_cells()) {
-    _volume_field.resize(phi.mesh().n_cells());
 
-    for (const auto& cell : phi.mesh().cells()) {
-        _volume_field[cell.id()] = cell.volume();
-    }
-}
-
-void ConstantScalar::apply() {
+template <>
+void ConstantScalar<SourceSign::Positive>::apply() {
     rhs() = _phi.data().array() * _volume_field.array();
 }
 
-// TODO: replace calls to apply interior by simply making coeff_matrix() an I matrix,
-// or -I matrix in case of SourceSign::Positive, during construction of ImplicitPhi
+template <>
+void ConstantScalar<SourceSign::Negative>::apply() {
+    rhs() = -_phi.data().array() * _volume_field.array();
+}
 
-//template <>
-//void ImplicitPhi<SourceSign::Positive>::apply_interior(const mesh::Cell& cell,
-//                                                       const mesh::Face& face) //NOLINT
-//{
-//    auto cell_id = cell.id();
-//    matrix(cell_id, cell_id) = -_coeff;
-//}
-//
-//template <>
-//void ImplicitPhi<SourceSign::Negative>::apply_interior(const mesh::Cell& cell,
-//                                                       const mesh::Face& face) // NOLINT
-//{
-//    auto cell_id = cell.id();
-//    matrix(cell_id, cell_id) = _coeff;
-//}
+template <>
+void ImplicitPhi<SourceSign::Positive>::apply() {
+    matrix().setIdentity();
+    matrix() *= -_coeff;
+}
+
+template <>
+void ImplicitPhi<SourceSign::Negative>::apply() {
+    matrix().setIdentity();
+    matrix() *= _coeff;
+}
 
 } // namespace prism::source
