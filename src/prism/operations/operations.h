1f2338c99f67eba942960bbb2d3a17af63fc1b97
eigemx <eigen.emara@gmail.com>
2023-11-19 16:53:02 +0200

    add operations to implement

--- src/prism/operations/operations.h
+++ src/prism/operations/operations.h
@@ -7,4 +7,13 @@ namespace prism::ops {
 // Calculates the divergence of a vector field U: ∇.U
 auto div(const VectorField& U, bool return_face_data = true) -> ScalarField;
 
+// Calculates the laplacian of a scalar field ϕ: ∇.∇ϕ
+auto laplacian(const ScalarField& phi, bool return_face_data = true) -> ScalarField;
+
+// Calculates the magnitude of a vector field U
+auto mag(const VectorField& U, bool return_face_data = true) -> ScalarField;
+
+// Calculates the curl of a vector field U
+auto curl(const VectorField& U, bool return_face_data = true) -> VectorField;
+
 } // namespace prism::ops
