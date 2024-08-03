148b6df809891f286be543484d47a0c5460edd11
eigemx <eigen.emara@gmail.com>
2024-08-03 10:31:40 +0300

    remove inline modifer from nonortho functions

--- src/prism/nonortho/nonortho.cpp
+++ src/prism/nonortho/nonortho.cpp
@@ -2,20 +2,18 @@
 
 namespace prism::nonortho {
 
-auto inline OverRelaxedCorrector::decompose(const Vector3d& Sf, const Vector3d& e) const
+auto OverRelaxedCorrector::decompose(const Vector3d& Sf, const Vector3d& e) const
     -> NonOrthoPair {
     Vector3d Ef = ((Sf.dot(Sf) / (e.dot(Sf) + EPSILON))) * e;
     return {Ef, Sf - Ef};
 }
 
-auto inline MinimumCorrector::decompose(const Vector3d& Sf, const Vector3d& e) const
-    -> NonOrthoPair {
+auto MinimumCorrector::decompose(const Vector3d& Sf, const Vector3d& e) const -> NonOrthoPair {
     Vector3d Ef = (e.dot(Sf)) * e;
     return {Ef, Sf - Ef};
 }
 
-auto inline OrthogonalCorrector::decompose(const Vector3d& Sf, const Vector3d& e) const
-    -> NonOrthoPair {
+auto OrthogonalCorrector::decompose(const Vector3d& Sf, const Vector3d& e) const -> NonOrthoPair {
     Vector3d Ef = Sf.norm() * e;
     return {Ef, Sf - Ef};
 }
