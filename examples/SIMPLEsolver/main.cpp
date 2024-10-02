f7e302dee5d7e777dbfdd32f9c19b683d4508cea
eigemx <eigen.emara@gmail.com>
2024-10-02 17:22:38 +0300

    fix divergence bug

--- examples/SIMPLEsolver/main.cpp
+++ examples/SIMPLEsolver/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <nlohmann/json.hpp>
 
+#include "prism/export.h"
 #include "prism/scheme/nonortho.h"
 
 using json = nlohmann::json;
@@ -41,8 +42,25 @@ auto readFields(const std::filesystem::path& fields_file) -> FoamFields {
     return {velocity_vec, pressure, {}};
 }
 
+auto readVelocityComponents(const std::vector<prism::Vector3d>& velocity)
+    -> std::array<VectorXd, 3> {
+    VectorXd u, w, v; // NOLINT
+    u.resize(velocity.size());
+    w.resize(velocity.size());
+    v.resize(velocity.size());
+
+    for (size_t i = 0; i < velocity.size(); i++) {
+        u[i] = velocity[i].x();
+        w[i] = velocity[i].y();
+        v[i] = velocity[i].z();
+    }
+
+    return {u, w, v};
+}
+
 auto main(int argc, char* argv[]) -> int {
     using namespace prism::scheme;
+    using namespace prism::scheme::convection;
 
     log::setLevel(log::Level::Info);
     if (argc < 2) {
@@ -57,18 +75,25 @@ auto main(int argc, char* argv[]) -> int {
     log::info("Loading mesh file `{}`...", unv_file_name);
     auto mesh = mesh::UnvToPMeshConverter(unv_file_name, boundary_file).toPMesh();
 
-    // read pressure field from json file
+    // read pressure field
     auto fields = readFields(fs::path(unv_file_name).parent_path() / "foam_fields.json");
     auto pressure_vec = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(fields.pressure.data(),
                                                                       fields.pressure.size());
+    // read velocity field
+    auto raw_velocity_array = readVelocityComponents(fields.velocity);
+    auto Ux = field::VelocityComponent("U_x", mesh, raw_velocity_array[0], Coord::X);
+    auto Uy = field::VelocityComponent("U_y", mesh, raw_velocity_array[1], Coord::Y);
+    auto Uz = field::VelocityComponent("U_z", mesh, raw_velocity_array[2], Coord::Z);
+    auto components = std::array {Ux, Uy, Uz};
 
     // set mesh fields
     auto mu = field::UniformScalar("mu", mesh, 1e-5);
-    auto U = field::Velocity("U", mesh, {0.0, 0.0, 0.0});
+    // auto U = field::Velocity("U", mesh, {0.0, 0.0, 0.0});
+    auto U = field::Velocity("U", mesh, components);
     auto P = field::Pressure("P", mesh, pressure_vec);
     auto rho = field::UniformScalar("rho", mesh, 1.0);
 
-    using div = convection::Upwind<field::UniformScalar, field::VelocityComponent>;
+    using div = Upwind<field::UniformScalar, field::VelocityComponent>;
     using laplacian = diffusion::
         Corrected<field::UniformScalar, nonortho::OverRelaxedCorrector, field::VelocityComponent>;
     using grad = source::Gradient<scheme::source::SourceSign::Negative, field::Pressure>;
@@ -83,20 +108,99 @@ auto main(int argc, char* argv[]) -> int {
                               grad(P, Coord::Y)     // = -∂p/∂y
     );
 
+    uEqn.boundaryHandlersManager().addHandler<eqn::boundary::NoSlip<eqn::Momentum>>();
+    vEqn.boundaryHandlersManager().addHandler<eqn::boundary::NoSlip<eqn::Momentum>>();
 
     auto U_solver = solver::BiCGSTAB<field::VelocityComponent,
                                      solver::ImplicitUnderRelaxation<field::VelocityComponent>>();
-    for (auto i = 0; i < 10; ++i) {
+
+    for (auto nOuterIter = 0; nOuterIter < 0; ++nOuterIter) {
         log::info("Solving y-momentum equation");
         U_solver.solve(vEqn, 10, 1e-4, 1.0);
 
         log::info("Solving x-momentum equation");
-        U_solver.solve(uEqn, 10, 1e-4, 1.0);
+        U_solver.solve(uEqn, 20, 1e-4, 1.0);
+
+        uEqn.zeroOutCoeffs();
+        vEqn.zeroOutCoeffs();
+
+        uEqn.updateCoeffs();
+        vEqn.updateCoeffs();
+
+        // calculate coefficients for the pressure equation
+        const auto& vol_vec = mesh.cellsVolumeVector();
+        const auto& uEqn_diag = uEqn.matrix().diagonal();
+        const auto& vEqn_diag = vEqn.matrix().diagonal();
+
+        auto D_data = std::vector<Matrix3d>();
+        D_data.reserve(mesh.cellCount());
+
+        auto Du = vol_vec.array() / (uEqn_diag.array() + prism::EPSILON);
+        auto Dv = vol_vec.array() / (vEqn_diag.array() + prism::EPSILON);
+
+        for (const auto& cell : mesh.cells()) {
+            auto i = cell.id();
+            // clang-format off
+            Matrix3d Di;
+            Di << Du[i], 0,     0,
+                  0,     Dv[i], 0,
+                  0,     0,     0;
+            // clang-format on
+            D_data.emplace_back(Di);
+        }
+
+        auto D = prism::field::Tensor("D", mesh, D_data);
+
+        // Rhie-Chow interpolation for velocity face values
+        ops::correctRhieChow(U, D, P);
+
+        log::info("Exporting rhi-chow velocity field");
+        export_field_vtu(U.x(), "rhi-chow_ux.vtu");
+        export_field_vtu(U.y(), "rhi-chow_uy.vtu");
+
+        // pressure correction field created with same name as pressure field to get same boundary
+        // conditions without having to define P_prime in fields.json file.
+        auto P_prime = field::Pressure("P", mesh, 0.0);
+
+        using laplacian_p =
+            diffusion::Corrected<field::Tensor, nonortho::OverRelaxedCorrector, field::Pressure>;
+        using div_p = source::Divergence<source::SourceSign::Negative, field::Velocity>;
+
+        // pressure correction equation (density is dropped from both sides of the equation due to
+        // incompressibility assumption)
+        auto pEqn = eqn::Transport<field::Pressure>(laplacian_p(D, P_prime), // - ∇.(D ∇P_prime)
+                                                    div_p(U)                 // == - (∇.U)
+        );
+
+        auto p_solver =
+            solver::BiCGSTAB<field::Pressure, solver::ImplicitUnderRelaxation<field::Pressure>>();
+
+
+        log::info("Solving pressure correction equation");
+        p_solver.solve(pEqn, 10, 1e-5, 1.0);
+
+        log::info("Exporting pressure correction field");
+        export_field_vtu(P_prime, "P_prime.vtu");
+
+        // update velocity fields
+        for (const auto& cell : mesh.cells()) {
+            auto correction = D.valueAtCell(cell) * P_prime.gradAtCell(cell);
+            U.x()[cell.id()] += -correction[0];
+            U.y()[cell.id()] += -correction[1];
+        }
+
+        P.values() = P.values().array() + (0.85 * P_prime.values().array());
+
+        uEqn.zeroOutCoeffs();
+        vEqn.zeroOutCoeffs();
     }
 
+    export_field_vtu(ops::div(U), "divU.vtu");
     export_field_vtu(uEqn.field(), "solution_x.vtu");
     export_field_vtu(vEqn.field(), "solution_y.vtu");
+    export_field_vtu(P, "pressure.vtu");
     export_field_vtu(ops::grad(P, Coord::X), "gradP_x.vtu");
+
     /*
     auto vEqn = eqn::Momentum(div(rho, U, U.y()),   // ∇.(ρUv)
                               laplacian(mu, U.y()), // - ∇.(μ∇v)
--- src/prism/scheme/source.h
+++ src/prism/scheme/source.h
@@ -117,11 +117,12 @@ Divergence<Sign, Vector>::Divergence(Vector U) : IExplicitSource(U.mesh().cellCo
 
 template <SourceSign Sign, typename Vector>
 void inline Divergence<Sign, Vector>::apply() {
+    const auto& vol_field = _U.mesh().cellsVolumeVector();
     if constexpr (Sign == SourceSign::Positive) {
-        rhs() = ops::div(_U).values();
+        rhs() = ops::div(_U).values().array() * vol_field.array();
         return;
     }
-    rhs() = -ops::div(_U).values();
+    rhs() = -ops::div(_U).values().array() * vol_field.array();
 }
 
 template <SourceSign Sign, typename Field>
