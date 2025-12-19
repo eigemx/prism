d98bcfc772cef0080b982fb13ab3fc0e42c537ff
eigemx <eigen.emara@gmail.com>
2025-12-19 13:21:25 +0200

    minor refactoring for convection sources

--- src/prism/scheme/convection/convection_hr.cpp
+++ src/prism/scheme/convection/convection_hr.cpp
@@ -1,10 +1,11 @@
 #include "convection_hr.h"
+
 #include "prism/field/scalar.h"
 
 namespace prism::scheme::convection {
 auto phiAtDummyUpwind(SharedPtr<field::Scalar>& field,
                       const mesh::Cell& cell,
-                      const mesh::Cell& downwind) -> double {
+                      const mesh::Cell& downwind) -> f64 {
     // The following is based on equation (12.66) from Moukalled et. al (2016)
     auto phi_downwind = field->valueAtCell(downwind);
     auto d_CD = downwind.center() - cell.center();
@@ -13,7 +14,7 @@ auto phiAtDummyUpwind(SharedPtr<field::Scalar>& field,
 
 auto phiTilde(SharedPtr<field::Scalar>& field,
               const mesh::Cell& cell,
-              const mesh::Cell& downwind) -> double {
+              const mesh::Cell& downwind) -> f64 {
     // The following is based on equation (12.1) from Moukalled et. al (2016)
     const auto phi_upwind = phiAtDummyUpwind(field, cell, downwind);
     auto a = field->valueAtCell(cell) - phi_upwind;
