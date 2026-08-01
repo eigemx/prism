7928c56068967618681ef4c9ba9dcfedc4813fb5
eigemx <eigen.emara@gmail.com>
2026-08-01 09:48:55 +0300

    Remove unused VelocityInlet scheme boundary handler

--- src/prism/scheme/boundary.h
+++ src/prism/scheme/boundary.h
@@ -18,49 +18,42 @@ template <typename Scheme>
 class Fixed : public ISchemeBoundaryHandler<Scheme> {
   public:
     void apply(Scheme& scheme, const mesh::BoundaryPatch& patch) override = 0;
-    auto inline name() const -> std::string override { return "fixed"; }
+    auto name() const -> std::string override { return "fixed"; }
 };
 
 template <typename Scheme>
 class NoSlip : public ISchemeBoundaryHandler<Scheme> {
   public:
     void apply(Scheme& scheme, const mesh::BoundaryPatch& patch) override = 0;
-    auto inline name() const -> std::string override { return "no-slip"; }
-};
-
-template <typename Scheme>
-class VelocityInlet : public ISchemeBoundaryHandler<Scheme> {
-  public:
-    void apply(Scheme& scheme, const mesh::BoundaryPatch& patch) override = 0;
-    auto inline name() const -> std::string override { return "velocity-intlet"; }
+    auto name() const -> std::string override { return "no-slip"; }
 };
 
 template <typename Scheme>
 class Outlet : public ISchemeBoundaryHandler<Scheme> {
   public:
     void apply(Scheme& scheme, const mesh::BoundaryPatch& patch) override = 0;
-    auto inline name() const -> std::string override { return "outlet"; }
+    auto name() const -> std::string override { return "outlet"; }
 };
 
 template <typename Scheme>
 class Symmetry : public ISchemeBoundaryHandler<Scheme> {
   public:
     void apply(Scheme& scheme, const mesh::BoundaryPatch& patch) override = 0;
-    auto inline name() const -> std::string override { return "symmetry"; }
+    auto name() const -> std::string override { return "symmetry"; }
 };
 
 template <typename Scheme>
 class FixedGradient : public ISchemeBoundaryHandler<Scheme> {
   public:
     void apply(Scheme& scheme, const mesh::BoundaryPatch& patch) override = 0;
-    auto inline name() const -> std::string override { return "fixed-gradient"; }
+    auto name() const -> std::string override { return "fixed-gradient"; }
 };
 
 template <typename Scheme>
 class ZeroGradient : public ISchemeBoundaryHandler<Scheme> {
   public:
     void apply(Scheme& scheme, const mesh::BoundaryPatch& patch) override = 0;
-    auto inline name() const -> std::string override { return "zero-gradient"; }
+    auto name() const -> std::string override { return "zero-gradient"; }
 };
 
 } // namespace prism::scheme::boundary
